b305c0ab64996b09e0b397f7de6ab3c644d57b52
Andrew J Kim <andrewk745@gmail.com>
2020-06-08 21:36:28 -0400

    added login component

---
+++ components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import { View, Text, StyleSheet } from 'react-native'
+
+
+
+
+export const Login = ({ }) => {
+    return (
+        <View>
+            <Text>Login to start queueing up for lines</Text>
+        </View>
+    )
+}
