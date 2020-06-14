920529dd882811bdc9aee85b0b7d996c973b1236
Andrew J Kim <andrewk745@gmail.com>
2020-06-14 15:58:23 -0400

    added icons to login page

--- components/Login.js
+++ components/Login.js
@@ -1,8 +1,8 @@
 import React from 'react' 
 import { View, Text, StyleSheet } from 'react-native'
 import { Input } from 'react-native-elements'
-import Icon from 'react-native-vector-icons/FontAwesome'
-
+import Icon from 'react-native-vector-icons/Fontisto'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
 
@@ -12,9 +12,23 @@ export const Login = ({ }) => {
             <Text>Login to start queueing up for lines</Text>
             <Input
             placeholder='Email'
+            leftIcon={
+                <Icon
+                    name='email'
+                    size={24}
+                    color='black'
+                />
+            }
             />
             <Input
             placeholder='Password'
+            leftIcon={
+                <Icon
+                    name='onepassword'
+                    size={24}
+                    color='black'
+                />
+            }
             />     
         </View>
     )
